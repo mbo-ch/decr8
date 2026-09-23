1005ced06b69acccaff5e09defca1526d34f7b15
ﮩـﮩﮩ٨ـ🫀ﮩ٨ـﮩﮩ٨ـ <aviciishawn@gmail.com>
2026-09-23 13:46:01 +0300

    Change API ID and hash in pyrogram_quickstart.py
    Updated API credentials for Pyrogram client.

--- pyrogram_quickstart.py
+++ pyrogram_quickstart.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
-api_id = 314504
-api_hash = "8c64c308e6f0186d495ae1e92a1c228d"
+api_id = 123456
+api_hash = "api_hash"
 
 app = Client("decr8_g-host", api_id=api_id, api_hash=api_hash)
 
